c17eea1df53ea44cb5d7bbacec1a2a9106ecc188
wheatwizard <olivee3@rpi.edu>
2018-01-09 16:38:38 -0500

    Replaced ArgumentGroups with lists

--- trees.hs
+++ trees.hs
@@ -1,14 +1,7 @@
 type Object = Int
 
-data ArgumentGroup a =
- None |
- Single a |
- Double a a |
- Triplet a a a
- deriving Show
-
 data Functoid a b = Functoid {
- implementation :: (ArgumentGroup a -> b),
+ implementation :: ([a] -> b),
  arity :: Int
 }
 
@@ -18,19 +11,16 @@ instance Functor (Functoid x) where
 --(<$) = fmap . const 
 
 instance Applicative (Functoid a) where
- pure x = Functoid (\None -> x) 0
+ pure x = Functoid (\[] -> x) 0
  
-
-wrap :: (ArgumentGroup a -> b) -> (ArgumentGroup a -> Functoid a b)
-wrap impl argG = Functoid (\None -> impl $ argG ) 0
+wrap :: ([a] -> b) -> ([a] -> Functoid a b)
+wrap impl argG = Functoid (\[] -> impl $ argG ) 0
 
 stitch :: [Functoid a a] -> [Functoid a a] -> [Functoid a a]
 stitch [] [] = []
 stitch (x@(Functoid _ 0):xs) ys = x : stitch xs ys
-stitch ((Functoid impl 1):xs) ((Functoid y 0):ys) = wrap impl (Single $ y None) : stitch xs ys
-stitch ((Functoid impl 2):xs) ((Functoid y1 0):(Functoid y2 0):ys) = wrap impl (Double (y1 None) (y2 None)) : stitch xs ys
-stitch ((Functoid impl 3):xs) ((Functoid y1 0):(Functoid y2 0):(Functoid y3 0):ys) = wrap impl (Triplet (y1 None) (y2 None) (y3 None)) : stitch xs ys
+stitch ((Functoid impl ar):xs) ys = wrap impl (map (($[]).implementation) $ take ar ys) : stitch xs (drop ar ys)
 
 evaluate :: [[Functoid a a]] -> a
 evaluate (x:y:rest) = evaluate $ stitch y x : rest
-evaluate [[Functoid impl 0]] = impl None 
+evaluate [[Functoid impl 0]] = impl [] 
