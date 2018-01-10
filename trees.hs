3c9edeaac679d025ed4d81efc9afb93523a20aff
1000000000 <lthyde@gmail.com>
2018-01-09 22:16:50 -0500

    Reworked entire code

--- trees.hs
+++ trees.hs
@@ -1,29 +1,15 @@
-type Object = Int
 
-data Functoid a b = Functoid {
- implementation :: ([a] -> b),
+data ProgTree a = ProgTree (Functoid a) [ProgTree a]
+
+data Functoid a = Functoid {
+ composition :: Functoid a -> Functoid a,
+ application :: [a] -> a,
  arity :: Int
 }
 
-instance Functor (Functoid x) where
---fmap :: (a -> b) -> Functoid x a -> Functoid x b
-  fmap f (Functoid impl ar) = Functoid (f . impl) ar
---(<$) = fmap . const 
+compose :: Functoid a -> Functoid a -> Functoid a
+compose (Functoid comp _ _) f = comp f
 
-instance Applicative (Functoid a) where
- pure x = Functoid (\[] -> x) 0
-
-infixr 9 <.>
-(<.>) :: Functoid a b -> Functoid a a -> Functoid a b
-(Functoid impl1 ar1) <.> (Functoid impl2 ar2) = Functoid newImpl (ar1 + ar2 - 1)
- where newImpl l
-        | length l == ar1 + ar2 - 1 = impl1 $ impl2 (drop (ar1 - 1) l) : take (ar1 - 1) l
-
-stitch :: [Functoid a a] -> [Functoid a a] -> [Functoid a a]
-stitch [] [] = []
-stitch (x@(Functoid _ 0):xs) ys = x : stitch xs ys
-stitch ((Functoid impl ar):xs) ys = pure (impl $ map (($[]).implementation) $ take ar ys) : stitch xs (drop ar ys)
-
-evaluate :: [[Functoid a a]] -> a
-evaluate (x:y:rest) = evaluate $ stitch y x : rest
-evaluate [[Functoid impl 0]] = impl [] 
+flatten :: ProgTree a -> Functoid a
+flatten (ProgTree f []) = f
+flatten (ProgTree f ls) = foldl compose f $ map flatten ls
