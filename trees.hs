b824a882ba1a6c1c189d317adda8342190625a47
wheatwizard <olivee3@rpi.edu>
2018-01-09 23:11:10 -0500

    Functoid compositions are now binary

--- trees.hs
+++ trees.hs
@@ -1,15 +1,13 @@
+import Control.Monad
 
 data ProgTree a = ProgTree (Functoid a) [ProgTree a]
 
 data Functoid a = Functoid {
- composition :: Functoid a -> Functoid a,
+ composition :: Functoid a -> Functoid a -> Functoid a,
  application :: [a] -> a,
  arity :: Int
 }
 
-compose :: Functoid a -> Functoid a -> Functoid a
-compose (Functoid comp _ _) f = comp f
-
 flatten :: ProgTree a -> Functoid a
 flatten (ProgTree f []) = f
-flatten (ProgTree f ls) = foldl compose f $ map flatten ls
+flatten (ProgTree f ls) = foldl (join composition) f $ map flatten ls
