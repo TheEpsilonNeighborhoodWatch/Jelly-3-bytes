b58cf0768706895304de4704c9507e32c542a4d9
wheatwizard <olivee3@rpi.edu>
2018-01-11 17:20:20 -0500

    Added a few integer based functions

--- stringparse.hs
+++ stringparse.hs
@@ -9,8 +9,25 @@ tokenparse :: String -> Functoid Object
 tokenparse "+" = (Functoid standardCompose (\[a,b] -> a + b) 2)
 tokenparse "*" = (Functoid standardCompose (\[a,b] -> a * b) 2)
 tokenparse "-" = (Functoid standardCompose (\[a,b] -> a - b) 2)
+tokenparse "^" = (Functoid standardCompose (\[a,b] -> a ^ b) 2)
+tokenparse "/" = (Functoid standardCompose (\[a,b] -> div a b) 2)
+tokenparse "%" = (Functoid standardCompose (\[a,b] -> mod a b) 2)
 tokenparse "0" = (Functoid standardCompose (\[] -> 0) 0)
 tokenparse "1" = (Functoid standardCompose (\[] -> 1) 0)
+tokenparse "2" = (Functoid standardCompose (\[] -> 2) 0)
+tokenparse "3" = (Functoid standardCompose (\[] -> 3) 0)
+tokenparse "4" = (Functoid standardCompose (\[] -> 4) 0)
+tokenparse "5" = (Functoid standardCompose (\[] -> 5) 0)
+tokenparse "6" = (Functoid standardCompose (\[] -> 6) 0)
+tokenparse "7" = (Functoid standardCompose (\[] -> 7) 0)
+tokenparse "8" = (Functoid standardCompose (\[] -> 8) 0)
+tokenparse "9" = (Functoid standardCompose (\[] -> 9) 0)
+
+-- Parser for the interpreter --
+semiparse :: String -> [Functoid Object]
+semiparse [] = []
+semiparse (a:x) = tokenparse (a:[]) : semiparse x
+
 
 -- Parser for the interpreter --
 semiparse :: String -> [Functoid Object]
