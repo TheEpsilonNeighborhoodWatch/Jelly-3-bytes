68c4c3e1882310262f7214590b72472f8c859461
wheatwizard <olivee3@rpi.edu>
2018-01-11 12:48:19 -0500

    Moved Functoid to its own module

---
+++ functoid.hs
@@ -0,0 +1,12 @@
+module Functoid (standardCompose, Functoid) where
+
+standardCompose :: Functoid a -> Functoid a -> Functoid a
+standardCompose (Functoid comp1 app1 ar1) (Functoid _ app2 ar2) = Functoid standardCompose newApp (ar1 + ar2 -1)
+ where newApp l
+        | length l == ar1 + ar2 - 1 = app1 $ app2 (drop (ar1 - 1) l) : take (ar1 - 1) l
+
+data Functoid a = Functoid {
+ composition :: Functoid a -> Functoid a -> Functoid a,
+ application :: [a] -> a,
+ arity :: Int
+}
